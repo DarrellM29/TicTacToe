7657a5ea2e9ff6d96f6d592133d847bafddd5ffb
Darrell Martin <darrellejm29@comcast.net>
2023-02-22 16:07:58 -0500

    Initial commit

---
+++ tictactoe.java
@@ -0,0 +1,217 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.control.Button;
+
+public class tictactoe extends Application {
+    // Indicate which player has a turn, initially it is the X player
+    private char whoseTurn = 'X';
+
+    // Create and initialize cell
+    private Cell[][] cell =  new Cell[3][3];
+
+    // Create and initialize a status label
+    private Label lblStatus = new Label("X's turn to play (X)");
+
+    Button restartButton = new Button("Restart");
+
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+
+        // Pane to hold cell
+        GridPane pane = new GridPane();
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                pane.add(cell[i][j] = new Cell(), j, i);
+
+        restartButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //reset the game state here
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        cell[i][j].setToken(' ');
+                        cell[i][j].getChildren().clear();
+                        cell[i][j].setStyle("-fx-background-color: white;");
+                        cell[i][j].setStyle("-fx-border-color: black");
+                    }
+                }
+                whoseTurn = 'X';
+                lblStatus.setText("X's turn to play");
+            }
+        });
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(pane);
+        borderPane.setBottom(lblStatus);
+        BorderPane.setAlignment(lblStatus, Pos.CENTER);
+        borderPane.setTop(restartButton);
+        BorderPane.setAlignment(restartButton, Pos.CENTER);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(borderPane, 450, 170);
+        primaryStage.setTitle("TicTacToe"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    /** Determine if the cell are all occupied */
+    public boolean isFull() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (cell[i][j].getToken() == ' ')
+
+                    return false;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+        cell[i][j].setStyle("-fx-background-color: yellow; -fx-border-color: black;");
+        return true;
+    }
+
+    /** Determine if the player with the specified token wins */
+    public boolean isWon(char token) {
+        for (int i = 0; i < 3; i++)
+            if (cell[i][0].getToken() == token
+                    && cell[i][1].getToken() == token
+                    && cell[i][2].getToken() == token) {
+
+                cell[i][0].setStyle("-fx-background-color: green;");
+                cell[i][1].setStyle("-fx-background-color: green;");
+                cell[i][2].setStyle("-fx-background-color: green;");
+
+                return true;
+            }
+
+        for (int j = 0; j < 3; j++)
+            if (cell[0][j].getToken() ==  token
+                    && cell[1][j].getToken() == token
+                    && cell[2][j].getToken() == token) {
+
+                cell[0][j].setStyle("-fx-background-color: green;");
+                cell[1][j].setStyle("-fx-background-color: green;");
+                cell[2][j].setStyle("-fx-background-color: green;");
+
+                return true;
+            }
+
+        if (cell[0][0].getToken() == token
+                && cell[1][1].getToken() == token
+                && cell[2][2].getToken() == token) {
+
+                cell[0][0].setStyle("-fx-background-color: green;");
+                cell[1][1].setStyle("-fx-background-color: green;");
+                cell[2][2].setStyle("-fx-background-color: green;");
+
+            return true;
+        }
+
+        if (cell[0][2].getToken() == token
+                && cell[1][1].getToken() == token
+                && cell[2][0].getToken() == token) {
+
+                cell[0][2].setStyle("-fx-background-color: green;");
+                cell[1][1].setStyle("-fx-background-color: green;");
+                cell[2][0].setStyle("-fx-background-color: green;");
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // An inner class for a cell
+    public class Cell extends Pane {
+        // Token used for this cell
+        private char token = ' ';
+
+        public Cell() {
+            setStyle("-fx-border-color: black");
+            this.setPrefSize(2000, 2000);
+            this.setOnMouseClicked(e -> handleMouseClick());
+        }
+
+        /** Return token */
+        public char getToken() {
+            return token;
+        }
+
+        /** Set a new token */
+        public void setToken(char c) {
+            token = c;
+
+            if (token == 'X') {
+                Line line1 = new Line(10, 10,
+                        this.getWidth() - 10, this.getHeight() - 10);
+                line1.endXProperty().bind(this.widthProperty().subtract(10));
+                line1.endYProperty().bind(this.heightProperty().subtract(10));
+                Line line2 = new Line(10, this.getHeight() - 10,
+                        this.getWidth() - 10, 10);
+                line2.startYProperty().bind(
+                        this.heightProperty().subtract(10));
+                line2.endXProperty().bind(this.widthProperty().subtract(10));
+
+                // Add the lines to the pane
+                this.getChildren().addAll(line1, line2);
+            }
+            else if (token == 'O') {
+                Ellipse ellipse = new Ellipse(this.getWidth() / 2,
+                        this.getHeight() / 2, this.getWidth() / 2 - 10,
+                        this.getHeight() / 2 - 10);
+                ellipse.centerXProperty().bind(
+                        this.widthProperty().divide(2));
+                ellipse.centerYProperty().bind(
+                        this.heightProperty().divide(2));
+                ellipse.radiusXProperty().bind(
+                        this.widthProperty().divide(2).subtract(10));
+                ellipse.radiusYProperty().bind(
+                        this.heightProperty().divide(2).subtract(10));
+                ellipse.setStroke(Color.BLACK);
+                ellipse.setFill(Color.WHITE);
+
+                getChildren().add(ellipse); // Add the ellipse to the pane
+            }
+        }
+
+        /* Handle a mouse click event */
+        private void handleMouseClick() {
+            // If cell is empty and game is not over
+            if (token == ' ' && whoseTurn != ' ') {
+                setToken(whoseTurn); // Set token in the cell
+
+                // Check game status
+                if (isWon(whoseTurn)) {
+                    lblStatus.setText(whoseTurn + " won! The game is over");
+                    whoseTurn = ' '; // Game is over
+                }
+                else if (isFull()) {
+                    lblStatus.setText("Draw! The game is over");
+                    whoseTurn = ' '; // Game is over
+                }
+                else {
+                    // Change the turn
+                    whoseTurn = (whoseTurn == 'X') ? 'O' : 'X';
+                    // Display whose turn
+                    lblStatus.setText(whoseTurn + "'s turn");
+                }
+            }
+        }
+    }
+
+    /**
+     * The main method is only needed for the IDE with limited
+     * JavaFX support. Not needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
